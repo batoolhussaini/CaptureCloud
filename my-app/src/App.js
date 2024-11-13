a0e7c308a0a80e9b6a6da004f23bcac91f9d1c36
batoolhussaini <batool.hussainisyeda@ucalgary.ca>
2024-11-13 13:07:48 -0700

    DOESNT WORK YET.. just pushing to publish

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,7 @@ import Statistics from './Pages/Statistics';
 import Trash from './Pages/Trash';
 import Sold from './Pages/Sold';
 import Upload from './Pages/Upload';
+import AllTags from './Pages/AllTags';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/trash" element={<Trash />} />
         <Route path="/sold" element={<Sold />} />
         <Route path="/upload" element={<Upload />} />
+        <Route path="/all-tags" component={<AllTags />} />
       </Routes>
     </Router>
   );
--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../Layout/Navbar.js';
 import logo from '../Assets/Logo/Logo.png';
+import Searchbar from '../Layout/Searchbar.js';
 
 function Home() {
   return (
@@ -9,8 +10,10 @@ function Home() {
         <Navbar /> 
       </div>
       <div className="flex justify-center">
-        <img src={logo} alt="Logo" className="mt-2 w-32" />
+        <img src={logo} alt="Logo" className="mt-2 w-32" /> 
       </div>
+      <h1 className="text-5xl text-center mb-6 text-[#6AABD2] mt-6">Home</h1> 
+      <Searchbar /> 
      
     </div>
   );
