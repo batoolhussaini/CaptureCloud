87d929a46c2fc48a84d0e11f69f014cec3a373ac
CarolineBasta <caroline.basta1@ucalgary.ca>
2024-11-18 17:21:53 -0700

    add trash page layout

--- my-app/src/Pages/Trash.js
+++ my-app/src/Pages/Trash.js
@@ -4,16 +4,22 @@ import logo from '../Assets/Logo/Logo.png';
 
 function Trash() {
     return (
-      <div className="flex flex-col">
-      <div className='fixed'>
-       <Navbar /> 
-     </div>
-     <div className="flex justify-center">
-       <img src={logo} alt="Logo" className="mt-2 w-32" />
-     </div>
-    
-   </div>
-      );
+        <div className="flex flex-col">
+            <div className="flex justify-center">
+                <img src={logo} alt="Logo" className="mt-2 w-32" />
+            </div>
+            <div className="fixed">
+                <Navbar />
+            </div>
+            <h1 className="text-6xl text-center mb-6 text-[#6AABD2] mt-6">Trash</h1>
+            <div className="flex">
+                <div className="flex justify-center">
+                    <img src={logo} alt="Logo" className="mt-2 w-32" />
+                </div>
+            </div> 
+        </div>
+    ); 
 }
+
 
 export default Trash;
