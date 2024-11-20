6a6e381dbfa9be825cc15abc9a62a59cd7f8edcc
arrushigupta <arushi_gupta@icloud.com>
2024-11-19 22:16:43 -0700

    navbar updates

--- my-app/src/Layout/Navbar.js
+++ my-app/src/Layout/Navbar.js
@@ -2,28 +2,27 @@ import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import profileIcon from '../Assets/Icons/account_circle.png';
 import homeIcon from '../Assets/Icons/Home.png';
-import homeSelectedIcon from '../Assets/Icons/home-selected.png';
-import janeSelectedIcon from '../Assets/Icons/Jane-selected-removebg-preview.png';
+import homeSelectedIcon from '../Assets/Icons/Home-selected.png';
+import janeSelectedIcon from '../Assets/Icons/Jane-selected.png';
 import soldIcon from '../Assets/Icons/photo.png';
-import soldSelectedIcon from '../Assets/Icons/Sold-selected-removebg-preview.png';
+import soldSelectedIcon from '../Assets/Icons/Sold-selected.png';
 import uploadIcon from '../Assets/Icons/Upload cloud.png';
-import uploadSelectedIcon from '../Assets/Icons/Upload-selected-removebg-preview.png';
+import uploadSelectedIcon from '../Assets/Icons/Upload-selected.png';
 import trashIcon from '../Assets/Icons/Trash 3.png';
-import trashSelectedIcon from '../Assets/Icons/Trash-selected-removebg-preview.png';
+import trashSelectedIcon from '../Assets/Icons/Trash-selected.png';
 import statsIcon from '../Assets/Icons/Pie chart.png';
-import statsSelectedIcon from '../Assets/Icons/Sats-selected-removebg-preview.png';
+import statsSelectedIcon from '../Assets/Icons/Stats-selected.png';
 import albumsIcon from '../Assets/Icons/folder_filled.png';
-import albumsSelectedIcon from '../Assets/Icons/Album-selected-removebg-preview.png';
+import albumsSelectedIcon from '../Assets/Icons/Album-selected.png';
 import logoutIcon from '../Assets/Icons/Log out.png';
 import line from '../Assets/Icons/Line 7.png';
 import chevronRightIcon from '../Assets/Icons/Chevron right.png';  
 
 function Navbar() {
-  const location = useLocation();  // Use location to determine current path
+  const location = useLocation(); 
   const [albumsVisible, setAlbumsVisible] = useState(false); 
   const [isChevronRotated, setIsChevronRotated] = useState(false);
 
-  //will add later when we display top 3 albums, it will track if the chevron icon has been rotatetd so that it displays the 3 albums
   const toggleAlbumsVisibility = () => {
     setAlbumsVisible(!albumsVisible);
     setIsChevronRotated(!isChevronRotated); 
@@ -33,38 +32,48 @@ function Navbar() {
   const isAlbumsPage = location.pathname === '/albums' || isAlbumDetailPage;
 
   return (
-    <div className="flex flex-col w-36 h-screen bg-Navbar-c text-text-c shadow-lg text-xs mt-0 pt-0">
-      <div className="flex items-center justify-center mb-12 mt-16">
+    <div className="flex flex-col w-36 h-screen bg-Navbar-c text-text-c shadow-lg text-xs pt-0 items-center " >
+      <div className="flex items-center justify-center " style={{ paddingTop: '6vh' }}>
         <Link to="/account" className="flex items-center space-x-2 font-bold mr-4">
           {location.pathname === '/account' ? (
-            <img src={janeSelectedIcon} alt="Profile" className="rounded-full transform translate-x-1 h-10" />
+            <div className="bg-selected h-12 w-24 flex items-center justify-center rounded-3xl">
+            <img src={janeSelectedIcon} alt="Profile" className="h-10 w-10" />
+            <span className="ml-2 text-white">Jane</span>
+          </div>
           ) : (
             <>
-              <img src={profileIcon} alt="Profile" className="h-8 w-8 rounded-full" />
+              <img src={profileIcon} alt="Profile" style={{ height: '5vh', width: '5vh' }} />
               <span>Jane</span>
             </>
           )}
         </Link>
       </div>
 
-      <div className="space-y-10 mt-10">
-        <Link to="/home" className="flex items-center space-x-2 ml-8 hover:font-bold">
-          {location.pathname === '/home' ? (
-            <img src={homeSelectedIcon} alt="Home" className="transform -translate-x-4" />
-          ) : (
-            <>
-              <img src={homeIcon} alt="Home" className="h-5 w-5" />
-              <span>Home</span>
-            </>
-          )}
-        </Link>
+      <div className="space-y-10 " style={{ paddingTop: '6vh'}}>
+      <Link to="/home" className="flex items-center space-x-2 ml-8 hover:font-bold">
+      {location.pathname === '/home' ? (
+        <div className="bg-selected h-8 w-28 flex items-center justify-center rounded-3xl" style={{ transform: 'translateX(-3vh)' }}>
+          <img src={homeSelectedIcon} alt="Home" className="h-6 w-6" />
+          <span className="ml-2 text-white">Home</span>
+        </div>
+      ) : (
+        <>
+          <img src={homeIcon} alt="Home" style={{ height: '3vh', width: '3vh' }} />
+          <span>Home</span>
+        </>
+      )}
+    </Link>
+
 
         <Link to="/sold" className="flex items-center space-x-2 ml-8 hover:font-bold">
           {location.pathname === '/sold' ? (
-            <img src={soldSelectedIcon} alt="Sold" className="transform -translate-x-4" />
+            <div className="bg-selected h-8 w-28 flex items-center justify-center rounded-3xl" style={{ transform: 'translateX(-3vh)' }}>
+            <img src={soldSelectedIcon} alt="Sold" className="h-6 w-6" />
+            <span className="ml-2 text-white">Sold</span>
+          </div>
           ) : (
             <>
-              <img src={soldIcon} alt="Sold" className="h-5 w-5" />
+              <img src={soldIcon} alt="Sold" style={{ height: '3vh', width: '3vh' }} />
               <span>Sold</span>
             </>
           )}
@@ -72,44 +81,57 @@ function Navbar() {
 
         <Link to="/upload" className="flex items-center space-x-2 ml-8 hover:font-bold">
           {location.pathname === '/upload' ? (
-            <img src={uploadSelectedIcon} alt="Upload" className="transform -translate-x-4" />
+            <div className="bg-selected h-8 w-28 flex items-center justify-center rounded-3xl" style={{ transform: 'translateX(-3vh)' }}>
+            <img src={uploadSelectedIcon} alt="Upload" className="h-6 w-6" />
+            <span className="ml-2 text-white">Upload</span>
+          </div>
           ) : (
             <>
-              <img src={uploadIcon} alt="Upload" className="h-5 w-5" />
+              <img src={uploadIcon} alt="Upload" style={{ height: '3vh', width: '3vh' }} />
               <span>Upload</span>
             </>
           )}
         </Link>
 
-        <Link to="/albums" className="flex items-center space-x-2 ml-8 hover:font-bold">
-          {isAlbumsPage ? (
-            <div className="flex items-center">
-              <img
-                src={chevronRightIcon}
-                alt="Chevron"
-                className={`h-4 w-4 cursor-pointer transform transition-transform duration-300 ${isChevronRotated ? 'rotate-90' : ''} -translate-x-7`} 
-                onClick={toggleAlbumsVisibility} 
-              />
-              <img src={albumsSelectedIcon} alt="Albums" className="transform -translate-x-8" />
-            </div>
-          ) : (
-            <>
-              <img src={albumsIcon} alt="Albums" className="h-5 w-5" />
-              <span>Albums</span>
-            </>
-          )}
-        </Link>
+        <Link to="/albums" className="flex items-center space-x-2  hover:font-bold">
+            {isAlbumsPage ? (
+              <div className="flex items-center"> 
+                <img
+                  src={chevronRightIcon}
+                  alt="Chevron"
+                  className="" 
+                  style={{
+                    height: '2vh',
+                    width: '2vh',
+                  }}                
+                />
+                <div className="bg-selected h-8 w-28 flex items-center justify-center rounded-3xl">
+                  <img src={albumsSelectedIcon} alt="Album" className="h-6 w-6" />
+                  <span className="ml-2 text-white">Albums</span>
+                </div>
+              </div>
+            ) : (
+              <>
+                <img src={albumsIcon} alt="Albums" className="ml-8"style={{ height: '3vh', width: '3vh' }} />
+                <span>Albums</span>
+              </>
+            )}
+          </Link>
 
-        <div className="flex items-center justify-center mb-12 hover:font-bold">
+
+        <div className="flex items-center justify-center hover:font-bold">
           <img src={line} alt="Separator" />
         </div>
 
         <Link to="/statistics" className="flex items-center space-x-2 ml-8 hover:font-bold">
           {location.pathname === '/statistics' ? (
-            <img src={statsSelectedIcon} alt="Statistics" className="transform -translate-x-4" />
+            <div className="bg-selected h-8 w-28 flex items-center justify-center rounded-3xl" style={{ transform: 'translateX(-2vh)' }}>
+              <img src={statsSelectedIcon} alt="Statistics" className="h-6 w-6" />
+              <span className="ml-2 text-white">Statistics</span>
+            </div>
           ) : (
             <>
-              <img src={statsIcon} alt="Statistics" className="h-5 w-5" />
+              <img src={statsIcon} alt="Statistics" style={{ height: '3vh', width: '3vh' }} />
               <span>Statistics</span>
             </>
           )}
@@ -117,20 +139,26 @@ function Navbar() {
 
         <Link to="/trash" className="flex items-center space-x-2 ml-8 hover:font-bold">
           {location.pathname === '/trash' ? (
-            <img src={trashSelectedIcon} alt="Trash" className="transform -translate-x-4" />
+            <div className="bg-selected h-8 w-28 flex items-center justify-center rounded-3xl" style={{ transform: 'translateX(-3vh)' }}>
+              <img src={trashSelectedIcon} alt="Trash" className="h-6 w-6" />
+              <span className="ml-2 text-white">Trash</span>
+            </div>
           ) : (
             <>
-              <img src={trashIcon} alt="Trash" className="h-5 w-5" />
+              <img src={trashIcon} alt="Trash" style={{ height: '3vh', width: '3vh' }} />
               <span>Trash</span>
             </>
           )}
         </Link>
-      </div>
 
-      <Link to="/login" className="flex items-center space-x-2 ml-8 mt-32 hover:font-bold">
-        <img src={logoutIcon} alt="Log Out" className="h-5 w-5" />
-        <span>Log Out</span>
-      </Link>
+        <div style={{ paddingTop: '12vh'}}>
+          <Link to="/login" className="flex items-center space-x-2 ml-8 hover:font-bold" >
+            <img src={logoutIcon} alt="Log Out" style={{ height: '3vh', width: '3vh' }} />
+            <span>Log Out</span>
+          </Link>
+        </div>
+        
+      </div>
     </div>
   );
 }
