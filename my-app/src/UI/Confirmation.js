11fda437bf2286abf29c00d62de0db693504702a
shree888 <shree.patel@ucalgary.ca>
2024-11-26 16:15:53 -0700

    added tooltips

--- my-app/src/UI/Confirmation.js
+++ my-app/src/UI/Confirmation.js
@@ -15,6 +15,7 @@ const Confirmation = ({ message, onConfirm }) => {
         <button
           onClick={onConfirm} 
           className="absolute top-3 right-3 text-bold text-gray-500 hover:text-gray-700 text-2xl"
+          title="Close"
         >
           x
         </button>
