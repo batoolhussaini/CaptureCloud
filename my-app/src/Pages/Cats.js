8230ad84ee02240c2d7a2c839d3e71b9c5531902
CarolineBasta <caroline.basta1@ucalgary.ca>
2024-12-06 23:34:42 -0700

    fix cats album page

--- my-app/src/Pages/Cats.js
+++ my-app/src/Pages/Cats.js
@@ -7,12 +7,9 @@ import photoIcon from '../Assets/Icons/Photo symbol.png';
 import infoIcon from '../Assets/Icons/Info icon.png';
 import editIcon from '../Assets/Icons/Edit pencil.png';
 import leftArrowIcon from '../Assets/Icons/Arrow left.png';
-import Button from '../UI/button';
-import uploadIcon from '../Assets/Icons/Upload.png';
 import ARpopup from '../UI/ARpopup';
 
 function Cats() {
-  const [photos, setPhotos] = useState([]);
   const [isRenamePopupOpen, setIsRenamePopupOpen] = useState(false);
   const fileInputRef = useRef(null);
   const navigate = useNavigate();
@@ -23,23 +20,6 @@ function Cats() {
   };
 
   const handleFileChange = (event) => {
-    const files = event.target.files;
-    if (files) {
-      const fileArray = Array.from(files);
-      const invalidFiles = fileArray.filter((file) => !file.type.startsWith('image/'));
-
-      if (invalidFiles.length > 0) {
-        alert('Please upload only image files.');
-        return;
-      }
-
-      if (fileArray.length > maxImagesPerUpload) {
-        alert(`You can only upload a maximum of ${maxImagesPerUpload} photos at a time.`);
-        return;
-      }
-
-      setPhotos((prevPhotos) => [...prevPhotos, ...fileArray]);
-    }
   };
 
   const handleEditAlbumName = () => {
@@ -89,50 +69,34 @@ function Cats() {
         </div>
       </div>
 
-      {photos.length === 0 && (
-        <div className="flex flex-col items-center mt-20">
-          <div className="border-2 border-dashed border-black rounded-2xl w-[33rem] h-56 flex flex-col items-center justify-center bg-[#F5F5F5] ml-10"> 
-            <img src={photoIcon} alt="Empty Folder Icon" className="h-20 w-20" />
-            <div className="flex items-center space-x-2">
-              <p className="text-black text-lg mt-4">Drag and drop, or</p>
-              <label
-                htmlFor="fileInput"
-                className="font-bold text-[#069DFA] hover:underline cursor-pointer text-lg mt-4"
-              >
-                select
-              </label>
-              <input
-                id="fileInput"
-                type="file"
-                name="image"
-                className="hidden"
-                onChange={handleFileChange}
-                multiple
-                accept="image/*"
-                ref={fileInputRef}
-              />
-            </div>
-            <div className="flex items-center space-x-2 mt-2">
-              <img src={infoIcon} alt="Information Icon" className="h-5 w-5" />
-              <p className="text-black-500 text-sm">{maxImagesPerUpload} photos per upload max</p>
-            </div>
+      <div className="flex flex-col items-center mt-20">
+        <div className="border-2 border-dashed border-black rounded-2xl w-[33rem] h-56 flex flex-col items-center justify-center bg-[#F5F5F5] ml-10"> 
+          <img src={photoIcon} alt="Empty Folder Icon" className="h-20 w-20" />
+          <div className="flex items-center space-x-2">
+            <p className="text-black text-lg mt-4">Drag and drop, or</p>
+            <label
+              htmlFor="fileInput"
+              className="font-bold text-[#069DFA] hover:underline cursor-pointer text-lg mt-4"
+            >
+              select
+            </label>
+            <input
+              id="fileInput"
+              type="file"
+              name="image"
+              className="hidden"
+              onChange={handleFileChange}
+              multiple
+              accept="image/*"
+              ref={fileInputRef}
+            />
+          </div>
+          <div className="flex items-center space-x-2 mt-2">
+            <img src={infoIcon} alt="Information Icon" className="h-5 w-5" />
+            <p className="text-black-500 text-sm">{maxImagesPerUpload} photos per upload max</p>
           </div>
         </div>
-      )}
-
-      {photos.length > 0 && (
-        <div className="grid grid-cols-4 gap-4 p-6 mt-10">
-          {photos.map((photo, index) => (
-            <div key={index} className="relative">
-              <img
-                src={URL.createObjectURL(photo)}
-                alt={`Cat Photo ${index + 1}`}
-                className="h-[200px] w-[200px] object-cover rounded-md shadow-md"
-              />
-            </div>
-          ))}
-        </div>
-      )}
+      </div>
 
       {isRenamePopupOpen && (
         <ARpopup onConfirm={handleRenameConfirm} onClose={handleRenameClose} />
